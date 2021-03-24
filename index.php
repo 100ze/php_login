654769a055b4243678a441106db0d20b345762d6
Wilton Ribeiro <wiltonribeiromaster@gmail.com>
2021-03-24 18:31:10 -0300

    mover página para pasta pages

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
 <?php
-  $tituloAba = "star sky";
-  require_once "templates/cabecalho.php";
-?>
-
-<header>
-  <h1>Você logou! ;)</h1>
-</header>
-
-<section>
-  <img src="loritta_uwu.gif">
-  <p>E é isso.</p>
-<section>
-
-<?php
-  require_once "templates/rodape.php";
+  header("location: /pages/logado.php");
+  die();
 ?>
---
+++ pages/logado.php
@@ -0,0 +1,17 @@
+<?php
+  $tituloAba = "star sky";
+  require_once "../templates/cabecalho.php";
+?>
+
+<header>
+  <h1>Você logou! ;)</h1>
+</header>
+
+<section>
+  <img src="img/loritta_uwu.gif">
+  <p>E é isso.</p>
+<section>
+
+<?php
+  require_once "templates/rodape.php";
+?>
