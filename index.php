0057a8d2e3803a65b4e75e23aa8cff0d080d3d8f
Wilton Ribeiro <wiltonribeiromaster@gmail.com>
2021-03-24 18:18:08 -0300

    criar página logado

--- index.php
+++ index.php
@@ -3,6 +3,15 @@
   require_once "templates/cabecalho.php";
 ?>
 
+<header>
+  <h1>Você logou! ;)</h1>
+</header>
+
+<section>
+  <img src="loritta_uwu.gif">
+  <p>E é isso.</p>
+<section>
+
 <?php
   require_once "templates/rodape.php";
 ?>
