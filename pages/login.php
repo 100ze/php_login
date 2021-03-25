06fbc17397bb16635949a40bd99cab9fa90f5c75
Wilton Ribeiro <wiltonribeiromaster@gmail.com>
2021-03-24 21:22:27 -0300

    logando o usuario

--- pages/login.php
+++ pages/login.php
@@ -1,11 +1,29 @@
 <?php
   require_once "../app/limparEntrada.php";
+  require_once "../app/logar.php";
 
   // variáveis
   $tituloAba = "Login";
   // armazenando endereço atual do script php
   // em execução
   $enderecoScriptPHP = limparEntrada($_SERVER["PHP_SELF"]);
+
+  // verificando se o usuário enviou algum dado
+  if(!empty($_POST['usuario']) && !empty($_POST['senha'])){
+    // limpando entradas
+    $usuario = limparEntrada($_POST['usuario']);
+    $senha = limparEntrada($_POST['senha']);
+    
+    // logando o usuário e armazenando o sucesso
+    // ou falha
+    $logado = logar($usuario, $senha);
+
+    if($logado){
+      // redirecionando caso esteja logado
+      header("location: logado.php");
+      die();
+    }
+  }
 
   // cabeçalho
   require_once "../templates/cabecalho.php";
