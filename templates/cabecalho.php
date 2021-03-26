5512b45b374416cccf5aa26699488b4450afc955
Wilton Ribeiro <wiltonribeiromaster@gmail.com>
2021-03-26 11:00:52 -0300

    adicionando arquivos css a página

--- templates/cabecalho.php
+++ templates/cabecalho.php
@@ -4,6 +4,8 @@ echo <<<HTML
     <title>$tituloAba</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/w3.css">
+    <link rel="stylesheet" href="css/site.css">
   </head>
   <body>
 HTML;
